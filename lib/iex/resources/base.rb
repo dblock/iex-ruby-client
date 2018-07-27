fe6f7f1627a58697073aa02d8568f623ab54cb13
rodolfobandeira <contato@rodolfobandeira.com>
2018-07-26 22:13:17 -0400

    Add key_stats

--- lib/iex/resources/base.rb
+++ lib/iex/resources/base.rb
@@ -8,6 +8,20 @@ module IEX
       rescue Faraday::ClientError => e
         raise IEX::Errors::ClientError, e.response
       end
+
+      def self.float_to_percentage(float_number)
+        return unless float_number.is_a? Numeric
+        return '+0.00%' if float_number.zero?
+        [
+          float_number > 0 ? '+' : '',
+          format('%.2f', float_number * 100),
+          '%'
+        ].join
+      end
+
+      def self.to_dollar(amount:, ignore_cents: true)
+        MoneyHelper.money_to_text(amount, 'USD', nil, no_cents: ignore_cents)
+      end
     end
   end
 end
