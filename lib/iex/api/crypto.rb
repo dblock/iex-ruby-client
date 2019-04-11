5b2269136cac9cc9f7cf6e2498e65a3c6ff8a6ac
Bing Xie <bing708@gmail.com>
2019-04-11 14:09:54 +1000

    Change the crypto API

--- lib/iex/api/crypto.rb
+++ lib/iex/api/crypto.rb
@@ -1,12 +1,12 @@
 module IEX
   module Api
     module Crypto
-      def self.get
-        connection.get.body
+      def self.get(symbol)
+        connection(symbol).get.body
       end
 
-      def self.connection
-        IEX::Api.default_connection 'market/crypto'
+      def self.connection(symbol)
+        IEX::Api.default_connection "crypto/#{symbol}/quote"
       end
     end
   end
--- lib/iex/resources/crypto.rb
+++ lib/iex/resources/crypto.rb
@@ -58,9 +58,9 @@ module IEX
       property 'ask_price', from: 'askPrice'
       property 'ask_size', from: 'askSize'
 
-      def self.get
-        IEX::Api::Crypto.get.map do |data|
-          new data
+      def self.get(crypto_symbol)
+        Base.symbol(crypto_symbol) do
+          new IEX::Api::Crypto.get(crypto_symbol)
         end
       end
     end
