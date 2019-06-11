21a409db6a980d74e58dfc5b025f985752a68a60
Jean-Luc <miamisp@mac.com>
2019-06-11 12:24:08 -1000

    Added Advanced Stats Module

--- lib/iex/resources.rb
+++ lib/iex/resources.rb
@@ -1,5 +1,6 @@
 require_relative 'resources/resource'
 
+require_relative 'resources/advanced_stats'
 require_relative 'resources/chart'
 require_relative 'resources/company'
 require_relative 'resources/dividends'
