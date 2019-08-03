bc38ff80f627831daf57e553f93c8dc36589a647
Bruno Joseph <bruno.joseph95@gmail.com>
2019-08-03 16:28:35 +0200

    Update properties for chart API endpoint

--- lib/iex/resources/chart.rb
+++ lib/iex/resources/chart.rb
@@ -5,15 +5,18 @@ module IEX
         property 'high'
         property 'low'
         property 'volume'
+        property 'open'
+        property 'close'
         property 'label'
-        property 'change_over_time', from: 'changeOverTime'
         property 'date', transform_with: ->(v) { Date.parse(v) }
       end
 
       class Default < Base
-        property 'open'
-        property 'close'
-        property 'unadjusted_volume', from: 'unadjustedVolume'
+        property 'u_high', from: 'uHigh'
+        property 'u_low', from: 'uLow'
+        property 'u_volume', from: 'uVolume'
+        property 'u_open', from: 'uOpen'
+        property 'u_close', from: 'uClose'
         property 'change'
         property 'change_percent', from: 'changePercent'
         property 'change_percent_s', from: 'changePercent', with: lambda { |v|
@@ -23,22 +26,14 @@ module IEX
             '%'
           ].join
         } # change in percent as a String with a leading + or - sign
-        property 'vwap'
+        property 'change_over_time', from: 'changeOverTime'
       end
 
       class OneDay < Base
         property 'minute'
-        property 'market_average', from: 'marketAverage'
-        property 'market_notional', from: 'marketNotional'
-        property 'market_number_of_trades', from: 'marketNumberOfTrades'
-        property 'market_high', from: 'marketHigh'
-        property 'market_low', from: 'marketLow'
-        property 'market_volume', from: 'marketVolume'
-        property 'market_change_over_time', from: 'marketChangeOverTime'
         property 'average'
         property 'notional'
         property 'number_of_trades', from: 'numberOfTrades'
-        property 'simplify_factor', from: 'simplifyFactor'
       end
     end
   end
