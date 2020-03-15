8a73c087602ba3deae0e3b5906896ecb0a51ce87
Bruno Joseph <bruno.joseph95@gmail.com>
2020-03-15 14:55:56 +0100

    Fix: KeyStats#week_52_change always returns nil

--- lib/iex/resources/key_stats.rb
+++ lib/iex/resources/key_stats.rb
@@ -8,7 +8,7 @@ module IEX
       property 'week_52_high_dollar', from: 'week52high', with: ->(v) { Resource.to_dollar(amount: v, ignore_cents: false) }
       property 'week_52_low', from: 'week52low'
       property 'week_52_low_dollar', from: 'week52low', with: ->(v) { Resource.to_dollar(amount: v, ignore_cents: false) }
-      property 'week_52_change'
+      property 'week_52_change', from: 'week52change'
       property 'week_52_change_dollar', from: 'week52change', with: ->(v) { Resource.to_dollar(amount: v, ignore_cents: false) }
       property 'dividend_yield', from: 'dividendYield'
       property 'ex_dividend_date', from: 'exDividendDate'
