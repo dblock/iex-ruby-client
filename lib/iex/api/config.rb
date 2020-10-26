0b5826200f89f815671ac954359a88fbd3574580
Aaron Rosenberg <aarongrosenberg@gmail.com>
2020-10-26 10:21:52 -0500

    Alphabetize attrs

--- lib/iex/api/config.rb
+++ lib/iex/api/config.rb
@@ -4,33 +4,34 @@ module IEX
       extend self
 
       ATTRIBUTES = %i[
-        proxy
-        user_agent
-        ca_path
         ca_file
-        logger
-        timeout
-        open_timeout
+        ca_path
         endpoint
+        logger
+        open_timeout
+        proxy
         publishable_token
-        secret_token
         referer
+        secret_token
+        timeout
+        user_agent
       ].freeze
 
       attr_accessor(*Config::ATTRIBUTES)
 
       def reset!
+        self.ca_file = defined?(OpenSSL) ? OpenSSL::X509::DEFAULT_CERT_FILE : nil
+        self.ca_path = defined?(OpenSSL) ? OpenSSL::X509::DEFAULT_CERT_DIR : nil
         self.endpoint = 'https://cloud.iexapis.com/v1'
         self.publishable_token = ENV['IEX_API_PUBLISHABLE_TOKEN']
         self.secret_token = ENV['IEX_API_SECRET_TOKEN']
         self.user_agent = "IEX Ruby Client/#{IEX::VERSION}"
-        self.ca_path = defined?(OpenSSL) ? OpenSSL::X509::DEFAULT_CERT_DIR : nil
-        self.ca_file = defined?(OpenSSL) ? OpenSSL::X509::DEFAULT_CERT_FILE : nil
-        self.proxy = nil
+
         self.logger = nil
-        self.timeout = nil
         self.open_timeout = nil
+        self.proxy = nil
         self.referer = nil
+        self.timeout = nil
       end
     end
 
