340d619bea4dcee78089480a97a3e6910aa132dc
Mathusan Selvarajah <mathusans52@gmail.com>
2021-07-23 16:27:20 -0400

    Transform milliseconds since epoch timestamp to date

--- lib/iex/resources/currency_rate.rb
+++ lib/iex/resources/currency_rate.rb
@@ -3,7 +3,7 @@ module IEX
     class CurrencyRate < Resource
       property 'symbol'
       property 'rate'
-      property 'timestamp'
+      property 'timestamp', transform_with: ->(v) { Date.strptime((v.to_f / 1000).to_s, '%s') }
     end
   end
 end
--- spec/iex/endpoints/fx_spec.rb
+++ spec/iex/endpoints/fx_spec.rb
@@ -12,7 +12,7 @@ describe IEX::Endpoints::FX do
 
       expect(subject[0].symbol).to eq 'USDCAD'
       expect(subject[0].rate).to eq 1.25674
-      expect(subject[0].timestamp).to eq 1_627_045_829_863
+      expect(subject[0].timestamp).to eq Date.strptime('1627045829.863', '%s')
     end
   end
 
@@ -25,15 +25,15 @@ describe IEX::Endpoints::FX do
 
       expect(subject[0].symbol).to eq 'USDCAD'
       expect(subject[0].rate).to eq 1.25674
-      expect(subject[0].timestamp).to eq 1_627_045_829_863
+      expect(subject[0].timestamp).to eq Date.strptime('1627045829.863', '%s')
 
       expect(subject[1].symbol).to eq 'USDGBP'
       expect(subject[1].rate).to eq 0.7262111386264443
-      expect(subject[1].timestamp).to eq 1_627_045_780_863
+      expect(subject[1].timestamp).to eq Date.strptime('1627045780.863', '%s')
 
       expect(subject[2].symbol).to eq 'USDJPY'
       expect(subject[2].rate).to eq 110.426
-      expect(subject[2].timestamp).to eq 1_627_045_825_365
+      expect(subject[2].timestamp).to eq Date.strptime('1627045825.365', '%s')
     end
   end
 
