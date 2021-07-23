142dc974f5940eb4a9113327b554e9ff73f81097
Mathusan Selvarajah <mathusans52@gmail.com>
2021-07-23 16:01:12 -0400

    Support single symbol without requiring array

--- lib/iex/endpoints/fx.rb
+++ lib/iex/endpoints/fx.rb
@@ -2,6 +2,7 @@ module IEX
   module Endpoints
     module FX
       def fx_latest(symbols, options = {})
+        symbols = Array(symbols)
         response = get('fx/latest', { token: publishable_token, symbols: symbols.join(',') }.merge(options))
         response.map { |row| IEX::Resources::CurrencyRate.new(row) }
       rescue Faraday::ResourceNotFound => e
--- spec/iex/endpoints/fx_spec.rb
+++ spec/iex/endpoints/fx_spec.rb
@@ -3,7 +3,20 @@ require 'spec_helper'
 describe IEX::Endpoints::FX do
   include_context 'client'
 
-  context 'known symbol', vcr: { cassette_name: 'fx/latest' } do
+  context 'known symbol', vcr: { cassette_name: 'fx/latest_one_symbol' } do
+    subject do
+      client.fx_latest('USDCAD')
+    end
+    it 'retrieves a list of CurrencyRates' do
+      expect(subject.length).to eq 1
+
+      expect(subject[0].symbol).to eq 'USDCAD'
+      expect(subject[0].rate).to eq 1.25674
+      expect(subject[0].timestamp).to eq 1_627_045_829_863
+    end
+  end
+
+  context 'known symbols', vcr: { cassette_name: 'fx/latest' } do
     subject do
       client.fx_latest(%w[USDCAD USDGBP USDJPY])
     end
