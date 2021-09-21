17c2b0d589a55fdca14eaf6bc1eb1c8169075347
Bogdan Guban <biguban@gmail.com>
2021-09-21 23:45:38 +0300

    Added base functionality

--- lib/iex/cloud/request.rb
+++ lib/iex/cloud/request.rb
@@ -1,8 +1,23 @@
 module IEX
   module Cloud
     module Request
+      STREAM_EVENT_DELIMITER = "\r\n\r\n".freeze
+
       def get(path, options = {})
         request(:get, path, options)
+      end
+
+      def get_stream(path, options = {})
+        buffer = ""
+        event_parser = Proc.new do |chunk|
+          events = (buffer + chunk).lines(STREAM_EVENT_DELIMITER)
+          buffer = events.last.end_with?(STREAM_EVENT_DELIMITER) ? '' : events.delete_at(-1)
+          events.each do |event|
+            yield JSON.parse(event.gsub(/\Adata: /, ''))
+          end
+        end
+
+        request(:get, path, { endpoint: sse_endpoint, request: { on_data: event_parser } }.merge(options))
       end
 
       def post(path, options = {})
@@ -20,8 +35,9 @@ module IEX
       private
 
       def request(method, path, options)
-        path = [endpoint, path].join('/')
+        path = [options.delete(:endpoint) || endpoint, path].join('/')
         response = connection.send(method) do |request|
+          request.options.merge!(options.delete(:request)) if options.key?(:request)
           case method
           when :get, :delete
             request.url(path, options)
@@ -29,7 +45,6 @@ module IEX
             request.path = path
             request.body = options.to_json unless options.empty?
           end
-          request.options.merge!(options.delete(:request)) if options.key?(:request)
         end
         response.body
       end
--- lib/iex/endpoints/quote.rb
+++ lib/iex/endpoints/quote.rb
@@ -6,6 +6,19 @@ module IEX
       rescue Faraday::ResourceNotFound => e
         raise IEX::Errors::SymbolNotFoundError.new(symbol, e.response[:body])
       end
+
+      # @param symbols - a list of symbols
+      # @param options[:interval] sets intervals such as 1Second, 5Second, or 1Minute
+      def stream_quote(symbols, options = {})
+        options[:symbols] = Array(symbols).join(',')
+        interval = options.delete(:interval)
+
+        get_stream("stocksUS#{interval}", { token: secret_token }.merge(options)) do |payload|
+          payload.each do |quote|
+            yield IEX::Resources::Quote.new(quote)
+          end
+        end
+      end
     end
   end
 end
