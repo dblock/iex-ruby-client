5b61afb1f9874d5bd2082cf3f8c9fa80c00f0903
Aaron Rosenberg <aarongrosenberg@gmail.com>
2022-07-20 10:50:54 -0500

    [cleanup] whitespace and inline blocks when easy

--- spec/iex/config/client_spec.rb
+++ spec/iex/config/client_spec.rb
@@ -1,20 +1,21 @@
 require 'spec_helper'
 
 describe IEX::Api::Config::Client do
-  before do
-    IEX::Api.config.reset!
-  end
+  before { IEX::Api.config.reset! }
+
   describe '#defaults' do
     it 'sets endpoint' do
       expect(IEX::Api.config.endpoint).to eq 'https://cloud.iexapis.com/v1'
     end
   end
+
   describe '#configure' do
     before do
       IEX::Api.configure do |config|
         config.endpoint = 'updated'
       end
     end
+
     it 'sets endpoint' do
       expect(IEX::Api.config.endpoint).to eq 'updated'
     end
